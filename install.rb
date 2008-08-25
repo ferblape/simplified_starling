28e874219ac4ba2b376a725bc76f5e0291b4da5a
Francesc Esplugas <fesplugas@intraducibles.net>
2008-08-25 13:46:17 +0200

    Cleaned install

--- install.rb
+++ install.rb
@@ -1,4 +1,10 @@
 require 'fileutils'
-require File.dirname(__FILE__) + '/lib/simplified_starling'
 
-Simplified::Starling.setup
+starling_plugin_folder = Dir.getwd + "/vendor/plugins/simplified_starling"
+starling_config = Dir.getwd + "/config/starling.yml"
+unless File.exist?(starling_config)
+  FileUtils.cp starling_plugin_folder + "/files/starling.yml.tpl", starling_config
+  puts "=> Copied starling configuration file."
+else
+  puts "=> Starling configuration file already exists."
+end
