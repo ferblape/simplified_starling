9a5a3bdd3ad76ed4c2985f2591205414462c0fde
Francesc Esplugas <fesplugas@intraducibles.net>
2008-08-25 13:59:22 +0200

    Use ERB to generate template

--- install.rb
+++ install.rb
@@ -1,10 +1,25 @@
-require 'fileutils'
+require 'erb'
+# require 'fileutils'
 
-starling_plugin_folder = Dir.getwd + "/vendor/plugins/simplified_starling"
+# starling_plugin_folder = Dir.getwd + "/vendor/plugins/simplified_starling"
+
 starling_config = Dir.getwd + "/config/starling.yml"
+starling_config_template = Dir.getwd + "/vendor/plugins/simplified_starling/files/starling.yml.erb"
+
 unless File.exist?(starling_config)
-  FileUtils.cp starling_plugin_folder + "/files/starling.yml.tpl", starling_config
+
+  pwd = Dir.getwd
+  template = File.read(starling_config_template)
+  result = ERB.new(template).result(binding)
+
+  starling_config_file = File.open(starling_config, 'w+')
+  starling_config_file.puts result
+  starling_config_file.close
+
   puts "=> Copied starling configuration file."
+
 else
+
   puts "=> Starling configuration file already exists."
+
 end
